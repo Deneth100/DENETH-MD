d4512172d9f1b0517e99879f8332dbbeae871b7d
Deneth100 <kingxdenethdev@gmail.com>
2025-04-05 22:47:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,53 +1,12 @@
-const fs = require("fs");
-require("dotenv").config();
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
 module.exports = {
-  //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "cMxjACSB#C1Et-dLolWpyVaHrpcf6sgEA48ILYL5bad-QgwlNzw4",
-  // ADD Your Session Id 
-  MONGODB: process.env.MONGODB || "",
-    // ADD Your MongoDB Database URL
-  PREFIX: process.env.PREFIX || ".",
-  // Add Your Custom Prefix 
-  mode: process.env.mode || "public",
-
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94761864425",
-  //========================================- OTHER - CONFIGS -=====================================================================
-  AUTO_VOICE: process.env.AUTO_VOICE || "false",
-  ANTI_BAD_WORDS_ENABLED: process.env.ANTI_BAD_WORDS_ENABLED || "true",
-  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-  ANTI_BAD_WORDS: (process.env.ANTI_BAD_WORDS || "pakayo,huththo").split(','),
-  ANTI_LINK: process.env.ANTILINK || "false",
-  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-  ALWAYS_TYPING: process.env.ALWAYS_TYPING || "false",
-  ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "false",
-  ANTI_BOT: process.env.ANTI_BOT || "true",
-  ANTI_DELETE: process.env.ANTI_DELETE || "true",
-  packname: process.env.packname || "🪄BHASHI",
-  author: process.env.author || "BHASHI x VISHWA",
-  //==========================================- API-CONFIGS -==========================================================
-  OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || "2d61a72574c11c4f36173b627f8cb177", //openweathermap.org
-  ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || "sk_6438bcc100d96458f8de0602aec662f4ba14b905fd090ad3", //elevenlabs.io
-  SHODAN_API: process.env.SHODAN_API || "cbCkidr6qd7AFVaYs56MuCouGfM8gFki", //developer.shodan.io
-  PEXELS_API_KEY: process.env.PEXELS_API_KEY || "39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH", // pexels.com
-  OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
-  PIXABAY_API_KEY: process.env.PIXABAY_API_KEY || "23378594-7bd620160396da6e8d2ed4d53", // pixabay.com
-  ZIPCODEBASE_API_KEY: process.env.ZIPCODEBASE_API_KEY || "0f94a5f0-6ea4-11ef-81da-579be4fb031c", // zipcodebase.com
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
-  GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
-  PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
-
-
-//------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------
-
-  
-  START_MSG: process.env.START_MSG || `Bhashi Conected` ,
-
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg",
-  MENU_IMG: process.env.MENU_IMG || "https://i.ibb.co/hRw1XK4/image.png",
-  MENU_MSG: process.env.MENU_MSG || `Menumsg`,
-    MENU_MS: process.env.MENU_MS || `menu 2`,
-
+SESSION_ID: process.env.SESSION_ID || "ZUgE2QJC#h9VEMz2YHlKSnuE2zkksL_swQ2lKBZmH3qnJooRgSJE"
+ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg",
+ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃bot name 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : Bot Name",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
