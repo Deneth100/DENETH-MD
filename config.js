6a19c7b9bdfac2c6a52465fed5d91cd618b282a8
Deneth100 <kingxdenethdev@gmail.com>
2025-04-05 22:33:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+const fs = require("fs");
+require("dotenv").config();
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ZUgE2QJC#h9VEMz2YHlKSnuE2zkksL_swQ2lKBZmH3qnJooRgSJE",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
