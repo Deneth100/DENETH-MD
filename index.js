b8477ecd7b39c27d61b655fcc4dd856b8d8207cc
Deneth100 <kingxdenethdev@gmail.com>
2025-04-05 22:57:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -81,10 +81,13 @@ conn.ev.on('messages.upsert', async(mek) => {
 mek = mek.messages[0]
 if (!mek.message) return	
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-await conn.readMessages([mek.key])
-}
-        if (config.ALWAYS_ONLINE == 'true'){
+ if (config.AUTO_READ_STATUS === 'true') {
+            if (mek.key && mek.key.remoteJid === 'status@broadcast') {
+            await conn.readMessages([mek.key])
+            }
+            }
+            if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+	    if (config.ALWAYS_ONLINE == 'true'){
                 await conn.sendPresenceUpdate('available', mek.key.remoteJid)
             }else{
                 await conn.sendPresenceUpdate('unavailable', mek.key.remoteJid)
