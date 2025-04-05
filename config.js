6ed63444b3525dd15a2ed71f391dd06787e22ba7
Deneth100 <kingxdenethdev@gmail.com>
2025-04-05 19:22:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,4 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ZUgE2QJC#h9VEMz2YHlKSnuE2zkksL_swQ2lKBZmH3qnJooRgSJE",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_OFFLINE: process.env.ALWAYS_OFFLINE || "true",
+ALWAYS_TYPING: process.env.ALWAYS_TYPING || "false",
+ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "true",
 };
