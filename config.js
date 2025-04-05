fc422f87eee492a393505fa42a741415cd580e7b
Deneth100 <kingxdenethdev@gmail.com>
2025-04-05 18:28:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ZUgE2QJC#h9VEMz2YHlKSnuE2zkksL_swQ2lKBZmH3qnJooRgSJE"
+SESSION_ID: process.env.SESSION_ID || "ZUgE2QJC#h9VEMz2YHlKSnuE2zkksL_swQ2lKBZmH3qnJooRgSJE",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
