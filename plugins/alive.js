621bf6d05b69c7dcec0cc52a8994e771464084c4
Deneth100 <kingxdenethdev@gmail.com>
2025-04-05 18:18:45 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,140 +1,45 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
-const os = require("os")
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+
 cmd({
     pattern: "alive",
+    react: "👋",
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let totalStorage = Math.floor(os.totalmem() / 1024 / 1024) + 'MB';
+        let freeStorage = Math.floor(os.freemem() / 1024 / 1024) + 'MB';
+        
+        let desc = `👋 Hey ${pushname},
 
-//============ping=======
-cmd({
-    pattern: "ping",
-    react: "⚡",
-    alias: ["speed"],
-    desc: "Check bot\'s ping",
-    category: "main",
-    use: '.ping',
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-var inital = new Date().getTime();
-let ping = await conn.sendMessage(from , { text: '```Pinging To index.js!!!```'  }, { quoted: mek } )
-var final = new Date().getTime();
-return await conn.edit(ping, '*Pong*\n *' + (final - inital) + ' ms* ' )
-} catch (e) {
-reply(`${e}`)
-console.log(e)
-}
-})
+I Aᴍ Aʟɪᴠᴇ Nᴏᴡ 
 
-//===========menu========
-cmd({
-    pattern: "menu",
-    desc: "To get the menu.",
-    react: "📜",
-    category: "main",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-    
-let menu = {
-main: '',
-download: '',
-group: '',
-owner: '',
-convert: '',
-ai: '',
-tools: '',
-search: '',
-fun: '',
-voice: '',
-other: ''
-};
+ɪ ᴀᴍ ᴀɴ ᴀᴜᴛᴏᴍᴀᴛᴇᴅ ꜱʏꜱᴛᴇᴍ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴛʜᴀᴛ ᴄᴀɴ ʜᴇʟᴘ ᴛᴏ ᴅᴏ ꜱᴏᴍᴇᴛʜɪɴɢ, ꜱᴇᴀʀᴄʜ ᴀɴᴅ ɢᴇᴛ ᴅᴀᴛᴀ / ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ᴏɴʟʏ ᴛʜᴏᴜɢʜ ᴡʜᴀᴛꜱᴀᴘᴘ
 
-for (let i = 0; i < commands.length; i++) {
-if (commands[i].pattern && !commands[i].dontAddCommandList) {
-menu[commands[i].category] += `.${commands[i].pattern}\n`;
- }
-}
+> ʙᴏᴛ ɴᴀᴍᴇ : ᴅᴇɴᴇᴛʜ-ᴍᴅ ᴠ1
+> ᴛᴏᴛᴀʟ ʀᴀᴍ : ${totalStorage}
+> ꜰʀᴇᴇ ʀᴀᴍ : ${freeStorage}
+> ᴏᴡɴᴇʀ : ᴋɪɴɢ X ᴅᴇɴᴇᴛʜᴅᴇᴠ®
 
-let madeMenu = `
-👋 𝐇𝐄𝐋𝐋𝐎, ${pushname}!
+🥰 𝗛𝗮𝘃𝗲 𝗮 𝗡𝗶𝗰𝗲 𝗗𝗮𝘆 🥰
 
-✨ 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝘁𝗼 your bot name ✨ 
-╭─「 ᴄᴏᴍᴍᴀɴᴅꜱ ᴘᴀɴᴇʟ」
-│◈ яυηтιмє * ${runtime(process.uptime())}
-│◈ σωηєя ηαмє * your name
-│◈ σωηєя ηυмвєя * your number 
-╰──────────●●►
-╭──────────●●►
- 📥 *𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.download}
-╰───────────●●►
-╭──────────●●►
- 👾 *𝐀𝐢 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.ai}
-╰───────────●●►
-╭──────────●●►
- 🔧 *𝐌𝐚𝐢𝐧 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.main}
-╰───────────●●►
-╭──────────●●►
- 🎉 *𝐅𝐮𝐧 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.fun}
-╰───────────●●►
-╭──────────●●►
- 🔄 *𝐂𝐨𝐧𝐯𝐞𝐫𝐭 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.convert}
-╰───────────●●►
-╭──────────●●►
- 🔍 *𝐒𝐞𝐚𝐫𝐜𝐡 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.search}
-╰───────────●●►
-╭──────────●●►
- 👥 *𝐆𝐫𝐨𝐮𝐩 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.group}
-╰───────────●●►
-╭──────────●●►
- 🔒 *𝐎𝐰𝐧𝐞𝐫 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.owner}
-╰───────────●●►
-╭──────────●●►
- ⚙️ *𝐎𝐭𝐡𝐞𝐫 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.other}
-╰───────────●●►
-╭──────────●●►
- 🛠️ *𝐓𝐨𝐨𝐥𝐬 𝐌𝐞𝐧𝐮*
-  ───────
- ${menu.tools}
-╰───────────●●►
+> ᴅᴇɴᴇᴛʜ-ᴍᴅ ʙʏ ᴋɪɴɢ X ᴅᴇɴᴇᴛʜᴅᴇᴠ®`;
 
-> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ your bot name*`
-
-return await conn.sendMessage(from,{image: {url: `https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg`},caption:madeMenu},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`𝔼𝕣𝕣𝕣𝕠𝕣`)
-}
-})
+        const sentMsg = await conn.sendMessage(from, {
+            image: { url: `https://github.com/deneth-hansaka-keerthirathna/DENETH-Media/blob/main/DENETH-MD%20V1.jpg?raw=true` },
+            caption: desc,  // Send the description as the caption
+            contextInfo: {
+                forwardingScore: 999,
+                isForwarded: true,
+            }
+        }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
