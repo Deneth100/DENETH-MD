bba8861f29c6d0cba2fc784ad6cd96af499eb937
Deneth100 <kingxdenethdev@gmail.com>
2025-04-05 22:17:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,9 @@ conn.ev.on('creds.update', saveCreds)
 conn.ev.on('messages.upsert', async(mek) => {
 mek = mek.messages[0]
 if (!mek.message) return	
-mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+mek.message = (getContentType(mek.message) === 'ephemeralMessage')
+  ? mek.message.ephemeralMessage.message
+  : mek.message;
 if (
   mek.key &&
   mek.key.remoteJid === 'status@broadcast' &&
